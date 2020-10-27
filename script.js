ff888a8eaaf145d82481747c87717819a24de05a
MCampos23 <camposvmiguelangel@gmail.com>
2020-10-27 18:48:24 +0100

    primer commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function sumar(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+ 
+    var suma = parseInt(num1) + parseInt(num2);
+ 
+    console.log(suma);
+   }
+
+function restar(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+ 
+    var resta = parseInt(num1) - parseInt(num2);
+ 
+    console.log(resta);
+   }
+
+function multiplicar(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+ 
+    var multiplicacion = parseInt(num1) * parseInt(num2);
+ 
+    console.log(multiplicacion);
+   }
+
+function dividir(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+ 
+    var division = parseInt(num1) / parseInt(num2);
+ 
+    console.log(division);
+   }
+
